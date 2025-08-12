bbf275dc743bd05bf51b234b4a00a21b85ee3e1e
MokaMoka77 <tbuiquang103@gmail.com>
2025-08-12 10:26:36 +0700

    Build shopee pipeline

--- dags/Shopee/crawl_shopee_rating.py
+++ dags/Shopee/crawl_shopee_rating.py
@@ -22,13 +22,13 @@ import json
 # }
 
 
-url = "https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&limit=2&offset=0&type=0&exclude_filter=1&filter_size=0&fold_filter=0&relevant_reviews=false&request_source=2&tag_filter=&variation_filters=&fe_toggle=%5B2%2C3%5D&shopid=487028617&itemid=29911154536&preferred_item_item_id=29911154536&preferred_item_shop_id=487028617&preferred_item_include_type=1" #
+url = "https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&limit=2&offset=0&type=0&exclude_filter=1&filter_size=0&fold_filter=0&relevant_reviews=false&request_source=2&tag_filter=&variation_filters=&fe_toggle=%5B2%2C3%5D&shopid=487028617&itemid=29911154536&preferred_item_item_id=29911154536&preferred_item_shop_id=487028617&preferred_item_include_type=1"
 headers = {
     "accept": "application/json",
     "accept-encoding": "gzip, deflate, br, zstd",
     "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
-    "af-ac-enc-dat": "3e8753c81dc9a5bd", #
-    "af-ac-enc-sz-token": "DM+ifFf8HpYhl5lHTLmh9A==|iPHWMQ8SnuPbEYf/qy/emnWuDPcDbwpY/QJdQx1xMzQhEv6hnfKBHsCgp7+YL8H0ccqfAaBDt4hjZnFj|E9S6bKuiBbShck7F|08|3", #
+    "af-ac-enc-dat": "b9fe1ae10eb1bc00", #
+    "af-ac-enc-sz-token": "2InOmY5pWjYVvWCPSi6+nw==|uPHWMQ8SnuPbEYf/qy/emnWuDPcDbwpY/QJdQ9YjMSshEv6hnfKBHsCgp7+YL8H0ccqfAaBDt4hjZnFj|E9S6bKuiBbShck7F|08|3", #
     "content-type": "application/json",
     "referer": "https://shopee.vn/shop/487028617/item/29911154536/rating",
     "sec-ch-ua": '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
@@ -41,11 +41,12 @@ headers = {
     "x-api-source": "rweb",
     "x-csrftoken": "oO4ZvdS1lhSae6iCm6hXnvyFJHm9Rfy8",
     "x-requested-with": "XMLHttpRequest",
-    "x-sap-ri": "116595688e22ddfbc2f3fb380701cc8c137565f2ca4c2205298a", #
-    "x-sap-sec": "O19b86mfTfztq4l+IIp6lNlvBSGmjWT95bEMooLrXIvV0ecQaVS3K0qC0Qo13/N5yOQZwoXxZslvUP743QlGmZtKQAFcCcRP9Z9l8O41S939PenE0D584YTfLmAt/0Ksod2FuASZiir1gvf/YkfjykJa9UCcMgQsdZsDPXZhdJ5AGZ2BRxFWo6xgSlDKBKBcRMDejhVrPtz1m8VOXGmhkwPO37yD6QmYDurAcX1AssXluFH6vNHtJbhGMacz0mlHb0K1Jdoabp0DLMQMJc7GcVvY0p15HJ0jdIRBN8ao5Auy8y4IuyGWzVj6GaX8hlRVh2yJU8zXbcJmL3gLPIISyEWQtYTT18XkH9XSwsEg6P9E5T0h/ndxmTLV4oNKMTqWExgF9jTkk00lqOlXVUM4n9+kjSCRBJ3alVKxIGr9VYnrVXcnHWj/GPJNs6iToaXJ/4CMvika2ltiz72yfXeehfgw2k75LubJnwd3u1RLASLyeJ/DfftjM44OzRzTZms5YVSEdeqjxvjGgOh5IGQeRpM9MSB+QG77Sl+VuATS+dAH8jU+LOtBw9WC3RSiGhtwZFRZ+/GDmXmVN5Q+kGd9EqnUlvYWgZm9ac5v/tK9vriEEiioilUEGxcX9veq4DRGm2RWgYmsvTUDMf9Tg1udu9tc0X+ieK6pRXdctDlapxLYY46LsZXzQtrKFk/xD/B4SGufQYEqZXDHLtPRfPMusodT5nVCfpwE5nwrfiH3EEYvXN7JyKBM05uNeT6oZ5eGQnPcHxksaicqOJ7pwob11gAivLaJwrGOGSRZpzt8fzBfxKIaAWG7iOxqp5jGJTEKbYrgyOalmG8Gj1QvxHYfbA82+C7MjMuCOwaZGEYZTaO7C3r2139sLfKRbQJWJla966Df2dsgDW/HfyFxmmExW/1EHk6rU5lSvt/tzgKCQR+/FcmB4FwOSVYEEUUR+8yFTXZrSj/MbnsdggX6vTYyrOPBi7yt1FKMK+894mXbjy2rh8cr5kA6sV51x/ouIy2CPLiqTmryprfQzqJTyCursoq5lcqQ5dQr/N9tbfiH1Mt4Sji5m+6NGPUZ3i8ImFr1rhUudi88woI4v64sB/cSPXyZkOrBY2SKV1nB9UHQnDz8p57X/sia2sRFq3oXIoP5XAamh31q+VTg3SAVavjlej1kC6xyHX6nC79T+n9OXwxkC4t0eb9YrLIA7UFXm317JuHcffjxXwFzVK6kRvJqRQcC3eYV79/I+RyXN0oNrahCA9bFFgfZxJ4mxiUk3G1usM9qwRgBgKYIVsBfKT5fUNLM/fxmzpcqVyNYgrUF0fDxzGf8xmsrpZZsesLfXQRn+0WYxWg59CtSPMJ2UInGRlIH1tyjBShlFjKosfDhrZSVP8Ig07cHXWgMDqz1IQL7METS/bsJhxAgqkmUkqykv6igpHe01waRLA9tve0QUWamXfnyQ68BEo76/0HaeKwJl4tsznSpKl1FEFlcJ//bAxFyt3XafJtUzte442l44bk4f3UjW7HrPnfFxBiztheW66KKeqAtyZsnvY946XgNqo3v2ieS5LabvoRZ8NL3uJuQ67DUxljqOjxPynhma4BrH6RhlHBmMjtTyHaNwVXzm1azWwoyJ9GqY23LT6KqwdFODmHMPluG0bNOXpCj5C4v6AAhtWd25lPiZbKL9fxe1TwHGYSnVRZxb49gbznGQDSsKuOJXO4U1I0As1fM1IxbkFopb7vM0FhKTTIuJM/VYvy+cbOt/NOQCmQzF9cnWdSRTzXDUj1tKqILg14iRvCdYzCnZZY1RQY/hhpOk1wixlxVb+Hgmvh6bFVbItUhdQiulTMdw0C334PYYDwcDkxwMw7vaQIQUhWdo/JKdSwx7ZGADrTvfWDdpq3nDYQ0Ln8G0Kd/TWv3/3qNuzWGD8iCSfemzNf0HWFU04NbwtcrV1umKsBx8Nle1BALqcSpqYFErAM9GeHXdyuJKP3HuHKUtGeQED3ZZ+kAdJIJrXZnV3Ii5PXavSGcOjI4/dvdtHpF6e4XhaipWqHt85aG/IWmpKuiu9sd/GBeTsZcLduvHI0vdRf5NTFazV8vTS9yK+sRYgIcT4AD5MlUdbaD9Hpa9qE0SOfvmWbi3BmxRQnwVhGiOMYDwEuntSV2KrdIY4jEl+1ICPrmqTNQusO5IIeDAciMzzDxTsW29Oj9FRY0U3/YQEeG3OrSSK==", #
+    "x-sap-ri": "b6be9968e86761152d0d243f07016bebe4b313294570afac0eab", #
+    "x-sap-sec": "biZH86l2bdvQj+2Wi4uWikjWU4q2iPwW14qEiOpWuyqIiOAWF4qCirwWT4qvikrWX4qXiJ2Wx4uRiJjWbduBiI2WC4uIiIuWm4uCi5wWy4uVi4pWjyqTi4uWi4uW+MwWi4uWi4qqv4uWiOJ/0TAQiduWLBTdrmp8i4tLN1Seu4VWiPDARnQWiduWi5jQPVBeWVKWiduWi4uWcow8i4uWi4Lir4VWiow8i4uIiTuWF4XWisu9i4uWi4uN+lwWp229i4tCiyuWH4AWiIwFi4uWi4tgf4uWikE6/4i7pgrik4uWiE5AWDnzFU4az/T4aW2hD5wVDtGD9kH/t1wp5Y593X8v0e78wn+8zoI3L42sC4UKGLiAbkzXt+NiPOa6WUcFvNign8fCHhf+TyFNbKLVTON8TETNY4AEPYe2FMkYNGvKUGzrVWYnc6ihw5elQYI6CHedhF6dHbW6Zoek/d63VsgXYufxdm9OXeP4SOAxF4qM1zzLbtTJLsyMuCZvqL0U6qEqVWdnEu9btANA+0EEduXEdGwKKyKHoyz6qmgVS0QW7TpWi4qWWFYL7h6gnDRWi4uPBY0dg27mZcPz5gjw+XYIwwnnV5UXggV34S27W2ynC8Q1MlSvJ9ArvoMUa9Il84qZi4uWzPacYB6tM0vihbLxqbx67TNHtBAviuwoTxChr2qh1V+kplw8WuzTcBFKXdGjdH6RSYBKUSkgCStzSUQHkyCMiqrspIdHXoqJufrLiq2L32DGLTbz/qw3LbC7NoEwi4uWf4uWiJHNqFHRbfjRjduWi5My5Favi4uWiTuWi5cMi4uWi4uWq4uWikQQRStK89paPlrYf2FGMVZ0wyOYkBn7fdm4KDdO9sM9w/c0nOPsnr6zIAzIzUicXzcWmrBeAPcrX5E+GmNDeyLHBVdkrjg/zz2PVMRBiQ5Xj7toQiO0WWgWq6ByprjpympWi4q7Q15cX1iXYDOuMJwY5z2oUV31qiOg9luHP2kO819UYMBTJJzZ++9QGYJua+OgmIuHQ126lp9u17wWi4qsLdSqZ25OcMWGym1aUdx77yOCAkbGPj/1lX7imaaR2lUhi8rkGUK8ig9qgDhoPVifSCWAXQiK/fVUcMhlZhTH9vwIcUy7EaXlY0MDwEx4O1BRSaXGpuwKvRV0Nq4XQnO0hbAAzCI+KSGtN3VytjATKsgQ/yUa0UCPUt5Y18oisr880bR9i4uWyvXWie2Wi4uwjSmdCXr61wlAuNiDBuCNNZ41JFr8Wso+fBj/yGEfDEUvTI7iqz+pKQFoyuwJr13gWdybFaDfcKl3bVa7DMOvjh+aTgfzMpVuAi2kcle8WrtsnIy6DewCEczyvw7ZSVX+N2bl7jmAgEhAiPxu3Tz9NWg3HdYjWwY4Q8ZSSIK2OrANAMHpXcbbE6h804+vNsoD6YGCiQb3WvXSENrQVmgvdXPGLILD21dpimUQI+JC4GUPgXxgvgZlrpSUWXrWi4t5IK0xAyeoFJNpZIG07wEWqBxt6cILLo9XSRn+13haelAnDqT2UvcxSSKnLL616bOZwBSOWhhJgil4NxicsayVoCyj+2vXGMG6v6HNqbUCL2sWRICpMWqz9O2WiJu8i4nA5YFXgm/W81EisAArRbrjA1skQA1bPyB5DDsT9iFl+8/VNUeZP+VQOCt2lZll32EzjvePdn7VBfpFJ6zWpD7M89mOOJMZZWqBH9v9QYpfrxAnXPBNPMtBxGovTbMc9Rq1jBMoWL7Y41R0JfwhOetTFlY6GJN+H0x5kG+797cIt11AHpT35Pq8UUXrvJ5PdvBogPYJBj/hqRA1SL51jJGtjkodn3cgZFhj3l/HdzcEG6FGjLsipIJpzOBTvRij+DNgwzDirdU0/Q9JIbHbjef8XpPXp114h+99jV1jYhXS2KahSZy4NeejdzEsBgKSZDdAl2tG2kwkQ+kZHxmdWQzGVv/6wD6AQVQL6vmN0sVSmWzu3NcATpi5mh/s9LzhsYTg/r+dtnEBx4qFh2+FIBkEfduWi53TOOA2Rdo3xyuWis== ", #
     "x-shopee-language": "vi",
     "x-sz-sdk-version": "1.12.21"
 }
+
 
 
 response = requests.get(url, headers=headers)
--- dags/shopee_pipeline_dag.py
+++ dags/shopee_pipeline_dag.py
@@ -11,20 +11,29 @@ import psycopg2
 RAW_DIR = "/opt/airflow/data/raw"
 PROCESSED_DIR = "/opt/airflow/data/processed"
 
+
+
+
 # Crawl Task
 def crawl_data():
     url = "https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&limit=2&offset=0&type=0&exclude_filter=1&filter_size=0&fold_filter=0&relevant_reviews=false&request_source=2&tag_filter=&variation_filters=&fe_toggle=%5B2%2C3%5D&shopid=487028617&itemid=29911154536&preferred_item_item_id=29911154536&preferred_item_shop_id=487028617&preferred_item_include_type=1"
     headers = {
+        "authority": "shopee.vn",
+        "method": "GET",
+        "path": "/api/v2/item/get_ratings?filter=0&flag=1&limit=2&offset=0&type=0&exclude_filter=1&filter_size=0&fold_filter=0&relevant_reviews=false&request_source=2&tag_filter=&variation_filters=&fe_toggle=%5B2%2C3%5D&shopid=487028617&itemid=29911154536&preferred_item_item_id=29911154536&preferred_item_shop_id=487028617&preferred_item_include_type=1",
+        "scheme": "https",
         "accept": "application/json",
         "accept-encoding": "gzip, deflate, br, zstd",
         "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
-        "af-ac-enc-dat": "7bcab5b2a6e0443d", #
-        "af-ac-enc-sz-token": "kjhjw6eDHmhfC91Z3zBj1w==|vPHWMQ8SnuPbEYf/qy/emnWuDPcDbwpY/QJdQ5Nt6CshEv6hnfKBHsCgp7+YL8H0ccqfAaBDt4hjZnFj|E9S6bKuiBbShck7F|08|3", #
+        "af-ac-enc-dat": "b9fe1ae10eb1bc00",
+        "af-ac-enc-sz-token": "2InOmY5pWjYVvWCPSi6+nw==|uPHWMQ8SnuPbEYf/qy/emnWuDPcDbwpY/QJdQ9YjMSshEv6hnfKBHsCgp7+YL8H0ccqfAaBDt4hjZnFj|E9S6bKuiBbShck7F|08|3",
         "content-type": "application/json",
-        "referer": "https://shopee.vn/shop/487028617/item/29911154536/rating",
+        "cookie": "_QPWSDCXHZQA=7c814c42-2a5a-411a-89bc-d10bb2eee6a0; REC7iLP4Q=f6d4eedb-5a4d-4959-a73c-c46279c961dc; SPC_F=ZYxGZMi246jCkaboTG6T7Ly13NtQgqSg; REC_T_ID=c64889aa-50de-11f0-8d48-123f085b8545; SPC_CLIENTID=ZYxGZMi246jCkabombtsekhtpzsiebxi; csrftoken=oO4ZvdS1lhSae6iCm6hXnvyFJHm9Rfy8; _sapid=7e338a90c442d0f87b6e4692f2bf0e7379d1cf8ff43746f2fe49f51c; SPC_SC_SA_TK=; SPC_SC_SA_UD=; SPC_SC_OFFLINE_TOKEN=; SC_SSO=-; SC_SSO_U=-; SPC_SC_SESSION=; SPC_ST=.SnpUQnF2b0xwWlNRV21od5BdZ7J2QCfeN6lDWfVkHiGUxdynr+zdVuyUbatvxVC6AQZGv+p3R+9/wL98d8Gtu4vARcMGi76vHa0wW+cgmOfoSmvtmtC69TF17RCsRstgULBsxW3y8WIrHELeSGsv0pAx5GMXrZXwGU7tD+zqsh6gtAzbhHGMUMQqFscyXyVgzdCfTaTQin9LaKLCLtP9V5Yw8ZcMUmFoHVhGZwA/gnEtOiL78nCc0KZDTgIgmlQe; SPC_U=1167420699; SPC_R_T_ID=/G8sm/hB6uaZQ7Fcb7grUUPiEsn+OhyaTtUJNBN1ign0JrbWm4WWhUEXP6jAOnfP9xsMB+JxyjHPPWZZTRiiEE0kWIALogQHLFFI8uIdTWnFdEffItYF154DuC/+nGGKKWyn+D0vrR71a92IxuheHMq0dRHgiC/6t2zSqsmjym4=; SPC_R_T_IV=enh6ODJwUkxtUnhvZEFzZA==; SPC_T_ID=/G8sm/hB6uaZQ7Fcb7grUUPiEsn+OhyaTtUJNBN1ign0JrbWm4WWhUEXP6jAOnfP9xsMB+JxyjHPPWZZTRiiEE0kWIALogQHLFFI8uIdTWnFdEffItYF154DuC/+nGGKKWyn+D0vrR71a92IxuheHMq0dRHgiC/6t2zSqsmjym4=; SPC_T_IV=enh6ODJwUkxtUnhvZEFzZA==; SPC_CDS_CHAT=0816a2e7-04e4-431b-86ae-041b3e0b1131; SPC_IA=1; SPC_SI=aUB3aAAAAABEUEZDUGpQbTo18wEAAAAAYXZXRTJYcnE=; SPC_SEC_SI=v1-aFE5S1B3bGN2Ung1ZzJYWMKt7qLulxa9HRVFUAYzIu4MJ/pcnkXR4SLHj6Ycb5/s68ITJhAH26v6FSqhU5F/pmeeZ0K08ZKZ2OesJYuFFkk=; shopee_webUnique_ccd=2InOmY5pWjYVvWCPSi6%2Bnw%3D%3D%7CuPHWMQ8SnuPbEYf%2Fqy%2FemnWuDPcDbwpY%2FQJdQ9YjMSshEv6hnfKBHsCgp7%2BYL8H0ccqfAaBDt4hjZnFj%7CE9S6bKuiBbShck7F%7C08%7C3; ds=9656ebd69c92afd7506917eb901c16f7; SPC_EC=.Y2pERFVRS1RkV096Mk9jbkcSc+4Iem5VvTZHuFpAY5J1bDri+4I4OpKZuLuV4T/raQljQ3rrAPWzMJE76SUypj3rMJzFcsjjzk63NBfTHg9nRfFLCkL4yMFUb8meHO7nwNe1QhMJ6BQjXWZY1zuGemay/3LCt7OHgY/8mGKGuHE5durKA1gth4KJW9hmsC8DcmvOlBBxxt4ZPhBKb0A2XtRpBtsz7lg6yJP0miA0rvGC+yovasb4jsIY3zd98DB9",
+        "priority": "u=1, i",
+        "referer": "https://shopee.vn/product/487028617/29911154536",
         "sec-ch-ua": '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
         "sec-ch-ua-mobile": "?1",
-        "sec-ch-ua-platform": '"Android"',  
+        "sec-ch-ua-platform": '"Android"',
         "sec-fetch-dest": "empty",
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-origin",
@@ -32,11 +41,12 @@ def crawl_data():
         "x-api-source": "rweb",
         "x-csrftoken": "oO4ZvdS1lhSae6iCm6hXnvyFJHm9Rfy8",
         "x-requested-with": "XMLHttpRequest",
-        "x-sap-ri": "74ac99686de9a58fb3cec4380701aac3031dcab9f97cc1dbc1ad", #
-        "x-sap-sec": "drWv2R977SYM+oeFN7ZFN0FFE7x7NLUFH7xhNK3Fo1xPNKgFI7xZN2UF57xtN0cFk7xTN3eF37Z+N3FF7SZcN/eFL7ZPN/ZFQ7ZZN6UFR7ZqN73F+1xXN7ZFN7ZFdtUFN7ZFN7aGe7ZFNMoe+c8MNSZF3U9mHU3DN7ZEAxkxo7GFNBuy62+FNSZFNdb5Jc23n/7FNSZFN7ZFMiUDN7ZFNRdX97GFN+UDN7ZPNaZFI7mFNBZAN7ZFN7bOdhZFHXeAN7bZN1ZF17gFN/UWN7ZFN7Z9M7ZFNNrSQmzdLiwEX7ZFNcS/4jj3XPodubHCeQviw0AK3fOlDSq5Tc+mpLmjnpllyk+897n8qQFtxIV4vQR13jjoSLxv4843QWglTG9f4hZohMQGIe0JmvNCvNJ82cUyPCe0oMOuksDDi+EuMaPP2geg9jx8ass0r1VmQFomCUY1FID/cNszuXKq4pHSebBhiVc+ElBLr9onlGVDEFoqo/w/G3+IqS1/1oE7TqniqyvsUTDmGsNqxRwV3mkHj0k7aJvjWmUl2XnL0dKUhoDFra3FN7ZI4fA6e8aggEPFN7b+DndAX6apilSTHkyjOWkYb4vpXNEua+RSi/RVi6ddzO2bL5pDR86ZnzcSiyFx11xCN7ZFy4BEAazjl8cO5MYjGtsyucSxSmF8caRmUwOwtzbG9y5bLbQB1hvA/pPz4bLylIJJTxFpP9bph/b2SNYOMuv10cVLhQBtDtUxFHZARYWvVpWfwWkkAgml2C5Q6YBjN7ZFM7ZFN3LM7Zg+33L++SZFNd7AHdwDN7ZFNaZFNd0ON7ZFN7ZFp7ZFN6HVRC0NgrNK1hOV+oiqAMmzITUysyNmNflcExBY7CqzbXQ2aMskK/Ta97Grpatm0e3BBn6v8v55hTW76xZwGkfei4/vUwVomfwCBnW2zszk8vEhn/HRo/IL+bAPr7FpyZ3FN7aeWxpDgq+3mJHYLyaiclXlt+LeTT8GURN/mhSgjqkNmf6zeyimOM0qZ5Psi48GYAN/WxR1THkc8yUFN7bfI4AL60BB/Uzc70Tr5LqWV1R2QwXqnZGww3mqbQ3qep0ajBKgRx7FK4+7SJ8/lY1f975dW6R7hrAdfhNnjysY/8Kble3F+CBMQlr69SGZEdcPQ6CnETc7CdEKZ4MDmHKgf0FnC3h7L4NdfWMFmUfbU6p0fsODS1BpCj9ezAIRx2mqwJmAN7ZFiMFFNceFN7a3zlnxj4kRfsSnl7zu6WoUB6+O05eqe0TbbydRGFsHi9CdP8pq7CIYIE3o+VqTPMOxYsdmVTKhCJY5/qnlALAxcB6+sxrgT623G7fRYakQR0ABZ6gP7jHiyAiTZhZJ6QSGpPNKguTGtd1eU4W6M+Xufcirw3Zkqa+NovNFe721f15rr0cDj6a0cDz7tsp4EIVDRXy4qCKHSTTNrlyKl31+jzK5r/s19mEylvKRxleuyYowNQ5Qizib2NU2zpcFN7atEPq28wkqFBGPdcwgXadYLHHRRL2GEWX7k+uAg78iSeudUGhDOK+9DGqfjwlKbXIZYxigNUD8PDv6YInGuspKsQ3qTg/CguxPGP3njb9FOW34mT9SjHSRXPGFN3ZDN7abyqlEUCgP1CO9N9sRKSWAP76pcdywPjjHh2iz/JSslQctfyYliCkUb6mBhHNgzJw1J5ysddZ8sPU0hxkNyqDuzBQzGYAEoA2RA9pEGMJYvBOp0KHMsr0gh/Vdp6ygusjljOstL66DInNV7ZB7qokVc7huXTBCnIO/f6b/JFgWBQ7yomNqhXMtcKKWhJXdFdvmc6X8Llc1XT+XL1puJiUqXdTxjsWgFm5wFXU5RCydgA4mL5w3brjipINv502ZxNRAXwUcL1P/scauF88peAkF8yxnY60R8qkMSeDAZ1sFwbjYHFaVbgTAKRBnYRPgB1bLN4xCsAK//OFL3wT42h6YnJvh4Rk4v1r/56D8345yiq/XkeEemPjdvEJjC9Fam66uRTNqTLJOPZrHnRVkbHktMSZFNd+zsj9NJciXCSZFNK==", #
+        "x-sap-ri": "b6be9968e86761152d0d243f07016bebe4b313294570afac0eab",
+        "x-sap-sec": "biZH86l2bdvQj+2Wi4uWikjWU4q2iPwW14qEiOpWuyqIiOAWF4qCirwWT4qvikrWX4qXiJ2Wx4uRiJjWbduBiI2WC4uIiIuWm4uCi5wWy4uVi4pWjyqTi4uWi4uW+MwWi4uWi4qqv4uWiOJ/0TAQiduWLBTdrmp8i4tLN1Seu4VWiPDARnQWiduWi5jQPVBeWVKWiduWi4uWcow8i4uWi4Lir4VWiow8i4uIiTuWF4XWisu9i4uWi4uN+lwWp229i4tCiyuWH4AWiIwFi4uWi4tgf4uWikE6/4i7pgrik4uWiE5AWDnzFU4az/T4aW2hD5wVDtGD9kH/t1wp5Y593X8v0e78wn+8zoI3L42sC4UKGLiAbkzXt+NiPOa6WUcFvNign8fCHhf+TyFNbKLVTON8TETNY4AEPYe2FMkYNGvKUGzrVWYnc6ihw5elQYI6CHedhF6dHbW6Zoek/d63VsgXYufxdm9OXeP4SOAxF4qM1zzLbtTJLsyMuCZvqL0U6qEqVWdnEu9btANA+0EEduXEdGwKKyKHoyz6qmgVS0QW7TpWi4qWWFYL7h6gnDRWi4uPBY0dg27mZcPz5gjw+XYIwwnnV5UXggV34S27W2ynC8Q1MlSvJ9ArvoMUa9Il84qZi4uWzPacYB6tM0vihbLxqbx67TNHtBAviuwoTxChr2qh1V+kplw8WuzTcBFKXdGjdH6RSYBKUSkgCStzSUQHkyCMiqrspIdHXoqJufrLiq2L32DGLTbz/qw3LbC7NoEwi4uWf4uWiJHNqFHRbfjRjduWi5My5Favi4uWiTuWi5cMi4uWi4uWq4uWikQQRStK89paPlrYf2FGMVZ0wyOYkBn7fdm4KDdO9sM9w/c0nOPsnr6zIAzIzUicXzcWmrBeAPcrX5E+GmNDeyLHBVdkrjg/zz2PVMRBiQ5Xj7toQiO0WWgWq6ByprjpympWi4q7Q15cX1iXYDOuMJwY5z2oUV31qiOg9luHP2kO819UYMBTJJzZ++9QGYJua+OgmIuHQ126lp9u17wWi4qsLdSqZ25OcMWGym1aUdx77yOCAkbGPj/1lX7imaaR2lUhi8rkGUK8ig9qgDhoPVifSCWAXQiK/fVUcMhlZhTH9vwIcUy7EaXlY0MDwEx4O1BRSaXGpuwKvRV0Nq4XQnO0hbAAzCI+KSGtN3VytjATKsgQ/yUa0UCPUt5Y18oisr880bR9i4uWyvXWie2Wi4uwjSmdCXr61wlAuNiDBuCNNZ41JFr8Wso+fBj/yGEfDEUvTI7iqz+pKQFoyuwJr13gWdybFaDfcKl3bVa7DMOvjh+aTgfzMpVuAi2kcle8WrtsnIy6DewCEczyvw7ZSVX+N2bl7jmAgEhAiPxu3Tz9NWg3HdYjWwY4Q8ZSSIK2OrANAMHpXcbbE6h804+vNsoD6YGCiQb3WvXSENrQVmgvdXPGLILD21dpimUQI+JC4GUPgXxgvgZlrpSUWXrWi4t5IK0xAyeoFJNpZIG07wEWqBxt6cILLo9XSRn+13haelAnDqT2UvcxSSKnLL616bOZwBSOWhhJgil4NxicsayVoCyj+2vXGMG6v6HNqbUCL2sWRICpMWqz9O2WiJu8i4nA5YFXgm/W81EisAArRbrjA1skQA1bPyB5DDsT9iFl+8/VNUeZP+VQOCt2lZll32EzjvePdn7VBfpFJ6zWpD7M89mOOJMZZWqBH9v9QYpfrxAnXPBNPMtBxGovTbMc9Rq1jBMoWL7Y41R0JfwhOetTFlY6GJN+H0x5kG+797cIt11AHpT35Pq8UUXrvJ5PdvBogPYJBj/hqRA1SL51jJGtjkodn3cgZFhj3l/HdzcEG6FGjLsipIJpzOBTvRij+DNgwzDirdU0/Q9JIbHbjef8XpPXp114h+99jV1jYhXS2KahSZy4NeejdzEsBgKSZDdAl2tG2kwkQ+kZHxmdWQzGVv/6wD6AQVQL6vmN0sVSmWzu3NcATpi5mh/s9LzhsYTg/r+dtnEBx4qFh2+FIBkEfduWi53TOOA2Rdo3xyuWis==",
         "x-shopee-language": "vi",
         "x-sz-sdk-version": "1.12.21"
     }
+
 
 
     response = requests.get(url, headers=headers)
@@ -72,21 +82,22 @@ def transform_data():
 
     print(f"Saved processed CSV to {csv_path}")
 
-# Load Task (ví dụ insert vào DB giả lập)
 def load_data():
+    import psycopg2
+
     # Lấy file CSV mới nhất trong thư mục processed
     files = sorted([f for f in os.listdir(PROCESSED_DIR) if f.endswith(".csv")])
     if not files:
         raise Exception("No processed files found")
     latest_file = os.path.join(PROCESSED_DIR, files[-1])
 
-    # Kết nối DB
+    # Kết nối DB local
     conn = psycopg2.connect(
-        host="postgres",  # hoặc localhost
+        host="host.docker.internal",  # để container Airflow truy cập PostgreSQL trên máy host
         port=5432,
-        database="mydb",
-        user="postgre",
-        password="123456"
+        database="mydb",              # đổi đúng tên DB local
+        user="postgres",              # user local
+        password="123456"             # password local
     )
     cursor = conn.cursor()
 
@@ -113,7 +124,8 @@ def load_data():
     cursor.close()
     conn.close()
 
-    print(f"Inserted data from {latest_file} into PostgreSQL successfully!")
+    print(f"Inserted data from {latest_file} into local PostgreSQL successfully!")
+
 
 # DAG
 with DAG(
