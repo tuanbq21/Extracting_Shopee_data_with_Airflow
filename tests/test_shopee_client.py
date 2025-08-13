c1ea4628c24754d8b895ab877cd4081bcf6427f9
MokaMoka77 <tbuiquang103@gmail.com>
2025-08-13 22:58:26 +0700

    update_test_file

--- tests/test_shopee_client.py
+++ tests/test_shopee_client.py
@@ -1,6 +1,12 @@
 import pytest
+import sys
+import os
 from unittest.mock import patch, MagicMock
-import dags.shopee_client
+
+# Thêm đường dẫn /opt/airflow vào sys.path để import dags
+sys.path.append(os.path.abspath("/opt/airflow"))
+
+import dags.shopee_client as shopee_client
 
 
 @patch("dags.shopee_client.requests.get")
@@ -14,16 +20,16 @@ def test_fetch_ratings_success(mock_get):
     }
     mock_get.return_value = mock_response
 
-    status_code, data = dags.shopee_client.shopee_client.fetch_ratings()
+    status_code, data = shopee_client.fetch_ratings()
 
-    mock_get.assert_called_once_with(dags.shopee_client.shopee_client.URL, headers=dags.shopee_client.shopee_client.HEADERS)
+    mock_get.assert_called_once_with(shopee_client.URL, headers=shopee_client.HEADERS)
     assert status_code == 200
     assert "ratings" in data
     assert data["ratings"][0]["comment"] == "Hàng tốt"
     assert data["ratings"][0]["rating_star"] == 5
 
 
-@patch("shopee_client.requests.get")
+@patch("dags.shopee_client.requests.get")
 def test_fetch_ratings_fail(mock_get):
     """Test khi API trả về lỗi (status 404)."""
     mock_response = MagicMock()
@@ -31,7 +37,7 @@ def test_fetch_ratings_fail(mock_get):
     mock_response.json.return_value = {"error": "Not Found"}
     mock_get.return_value = mock_response
 
-    status_code, data = dags.shopee_client.shopee_client.fetch_ratings()
+    status_code, data = shopee_client.fetch_ratings()
 
     assert status_code == 404
     assert "error" in data
